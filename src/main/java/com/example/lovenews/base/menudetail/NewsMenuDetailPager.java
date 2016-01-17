14c8af098b86b3af909178ba7058ce24c3fa3e75
wuyinlei <1069584784@qq.com>
2016-01-17 11:25:04 +0800

    处理ViewPager事件拦截机制的优化

--- src/main/java/com/example/lovenews/base/menudetail/NewsMenuDetailPager.java
+++ src/main/java/com/example/lovenews/base/menudetail/NewsMenuDetailPager.java
@@ -2,15 +2,17 @@ package com.example.lovenews.base.menudetail;
 
 import android.app.Activity;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.lovenews.R;
+import com.example.lovenews.activity.MainActivity;
 import com.example.lovenews.base.BaseMenuDetailPager;
 import com.example.lovenews.base.TabDetailPager;
 import com.example.lovenews.bean.NewsData;
-import com.example.lovenews.view.HorizontalViewPager;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
@@ -31,9 +33,9 @@ import java.util.List;
 /**
  * 菜单详情页---新闻
  */
-public class NewsMenuDetailPager extends BaseMenuDetailPager {
+public class NewsMenuDetailPager extends BaseMenuDetailPager implements ViewPager.OnPageChangeListener {
 
-    private HorizontalViewPager mViewPager;
+    private ViewPager mViewPager;
     private TabPageIndicator mIndicator;
 
     @ViewInject(R.id.iv_next)
@@ -46,16 +48,16 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
      */
     List<NewsData.NewsTabData> mNewsTabData;
 
-    public NewsMenuDetailPager(Activity activity,List<NewsData.NewsTabData> children) {
+    public NewsMenuDetailPager(Activity activity, List<NewsData.NewsTabData> children) {
         super(activity);
-        mNewsTabData= children;
+        mNewsTabData = children;
     }
 
     @Override
     public View initViews() {
 
         View view = View.inflate(mActivity, R.layout.menu_news_center_details, null);
-        mViewPager = (HorizontalViewPager) view.findViewById(R.id.viewPager);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
         ViewUtils.inject(this, view);
 
         /**
@@ -71,7 +73,7 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
      * 跳转到下一个页面
      */
     @OnClick(R.id.iv_next)
-    public void nextPage(View view){
+    public void nextPage(View view) {
         int currentItem = mViewPager.getCurrentItem();
         mViewPager.setCurrentItem(++currentItem);
     }
@@ -83,23 +85,55 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
 
         //初始化页签数据
         for (int i = 0; i < mNewsTabData.size(); i++) {
-            TabDetailPager tabDetailPager = new TabDetailPager(mActivity,mNewsTabData.get(i));
+            TabDetailPager tabDetailPager = new TabDetailPager(mActivity, mNewsTabData.get(i));
             mPagerList.add(tabDetailPager);
         }
 
         mViewPager.setAdapter(new MenuDetailAdapter());
+        /**
+         *   当ViewPager和Indicator在一起绑定的时候，如果给ViewPager设置滑动绑定事件
+         是不能的，要给Indicator设置滑动绑定事件
+         */
+
+        // mViewPager.setOnPageChangeListener(this);
+        mIndicator.setOnPageChangeListener(this);
 
         /**
          * 把viewpager设置给指示器
          *
          * 要在viewpager设置完成adapter之后才能设置
          */
+
         mIndicator.setViewPager(mViewPager);
 
 
     }
 
-    class MenuDetailAdapter extends PagerAdapter{
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        MainActivity mainUi = (MainActivity) mActivity;
+        SlidingMenu slidingMenu = mainUi.getSlidingMenu();
+        if (position == 0) {
+            slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        } else {
+            slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    /**
+     * ViewPager适配器
+     */
+    class MenuDetailAdapter extends PagerAdapter {
         @Override
         public int getCount() {
             return mPagerList.size();
@@ -107,7 +141,7 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view==object;
+            return view == object;
         }
 
         @Override

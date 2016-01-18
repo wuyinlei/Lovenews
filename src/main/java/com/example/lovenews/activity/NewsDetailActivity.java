571c2403e4a660b413f250cec6970eaeb5f6deed
wuyinlei <1069584784@qq.com>
2016-01-18 09:40:04 +0800

    集成ShareSDK分享，完成新闻的分享到其他平台

--- src/main/java/com/example/lovenews/activity/NewsDetailActivity.java
+++ src/main/java/com/example/lovenews/activity/NewsDetailActivity.java
@@ -19,6 +19,9 @@ import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class NewsDetailActivity extends AppCompatActivity {
 
     @ViewInject(R.id.ivBack)
@@ -43,6 +46,9 @@ public class NewsDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+        ShareSDK.initSDK(this);
+
         ViewUtils.inject(this);
         String url = getIntent().getStringExtra("url");
 
@@ -135,8 +141,44 @@ public class NewsDetailActivity extends AppCompatActivity {
                 showChooseDialog();
                 break;
             case R.id.ivShare:
+                showShare();
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ShareSDK.stopSDK(this);
+    }
+
+    private void showShare() {
+
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 
     private int mCurrentChooseItem;//记录当前选中的item   点击确定前
